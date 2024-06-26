f3413cfa7feae82e83a0c437cdc87ead7da122af
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-06-26 15:00:58 +0300

    Done with the Hero UI

---
+++ src/components/home/Hero.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./home.module.scss";
+import AuthNav from "./AuthNav";
+import { IoSearch } from "react-icons/io5";
+
+type Props = {};
+
+function Hero({}: Props) {
+  let isAuthenticated = false; //placeholder
+  return (
+    <div className={styles.hero}>
+      <div className={styles.nav}>
+        <h1>Image Hive</h1>
+        <AuthNav />
+      </div>
+      <div className={styles.searchSection}>
+        <h1>Upload Your Stunning Pictures, Share Them With The World</h1>
+        <div className={styles.searchHolder}>
+          <input type="text" placeholder="Search here..." name="search"></input>
+          <IoSearch className={styles.icon} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
