092464be32deba89d6ae66130e198cb2f7d47deb
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-06-26 15:00:23 +0300

    changed the structure of the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
-import styles from "./page.module.scss";
+import AuthModal from "@/components/AuthModal/AuthModal";
+import Hero from "@/components/home/Hero";
 
 import React from "react";
 
 type Props = {};
 
 function page({}: Props) {
-  return <div>page</div>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
 
 export default page;
