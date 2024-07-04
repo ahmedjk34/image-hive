3a756f5542ea442a2a6cdc884f9c243c2c7b68a2
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-07-04 16:05:20 +0300

    slight changes to footer styling

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import styles from "./footer.module.scss";
+import { FaGithub } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <h2 className={styles.title}>Image Hive</h2>
         <p className={styles.copy}>
           &copy; 2024 Image Hive. All rights reserved.
         </p>
-        <p className={styles.madeBy}>Made by ahmedjk34</p>
+        <p className={styles.madeBy}>
+          Made by ahmedjk34 <FaGithub />
+        </p>
       </div>
     </footer>
   );
