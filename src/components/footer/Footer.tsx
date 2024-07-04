d80253cf682bda9c08106034287c1cb4cb454a24
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-07-04 16:00:25 +0300

    added a footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import styles from "./footer.module.scss";
 
-type Props = {};
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>Image Hive</h2>
+        <p className={styles.copy}>
+          &copy; 2024 Image Hive. All rights reserved.
+        </p>
+        <p className={styles.madeBy}>Made by ahmedjk34</p>
+      </div>
+    </footer>
+  );
+};
 
-function footer({}: Props) {
-  return <div>footer</div>;
-}
-
-export default footer;
+export default Footer;
